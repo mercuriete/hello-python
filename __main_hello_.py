1262ba38f37d38f18a8cac557e613ab8804cdd39
mercuriete <mercuriete@gmail.com>
2018-02-21 22:45:01 +0100

    first commit and travis support

---
+++ __main_hello_.py
@@ -0,0 +1,14 @@
+"""
+This module is a hello world
+"""
+
+
+def main():
+    """
+    This function is the hello world function
+    """
+    print "Hello World"
+
+
+if __name__ == "__main__":
+    main()
